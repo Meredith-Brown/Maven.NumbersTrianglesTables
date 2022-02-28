677884e5de1bd06b8e706ca80c31aa2a3722f339
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-02-28 17:14:15 -0500

    Numbers-Even, Numbers-Odd complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,12 +3,22 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+    StringBuilder evenNumbers = new StringBuilder();
+    for (int i = start; i < stop; i++)
+        if (i % 2 == 0) {
+            evenNumbers.append(i);
+        }
+    String output = evenNumbers.toString();
+    return output;
     }
 
-
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNumbers = "";
+        for (int i = start; i < stop; i++)
+            if (i % 2 != 0) {
+                oddNumbers += i;
+            }
+        return oddNumbers;
     }
 
 
