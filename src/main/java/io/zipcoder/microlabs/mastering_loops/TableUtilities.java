54fb65434ea07b0d5fec8f9b47459a7b6545f694
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-01 15:45:43 -0500

    completed lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,25 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        int tableSize = 5;
+        return getMultiplicationTable(tableSize);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        int tableSize = 10;
+        return getMultiplicationTable(tableSize);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder output = new StringBuilder();
+        for(int w = 1; w <= tableSize; w++) {
+            for(int h = 1; h <= tableSize; h++) {
+                output.append(String.format("%3d", w * h));
+                output.append(" |");
+    }
+            output.append("\n");
+}
+        String table = output.toString();
+        return table;
     }
 }
